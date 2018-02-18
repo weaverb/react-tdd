99a6033f6cba2af423e24e379ea07323a3262215
Bryan Weaver <bryanweaver@outlook.com>
2018-02-18 12:36:12 -0600

    WIP: need to fix tests

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+});
 
 function ProductList(props) {
+  const { classes } = props;
   return (
-    <ul>
-      {props.products.map(p => (
-        <li key={p.id} onClick={() => props.onProductSelect(p)}>
-          {p.name} ({p.brand})
-        </li>
-      ))}
-    </ul>
+    <div className={classes.root}>
+      <List component="div">
+        {props.products.map(p => (
+          <ListItem button key={p.id} onClick={() => props.onProductSelect(p)}>
+            <ListItemIcon>
+              <i className="material-icons md-36">add_circle</i>
+            </ListItemIcon>
+            <ListItemText primary={`${p.name} (${p.brand})`} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
   );
 }
 
@@ -18,4 +34,4 @@ ProductList.propTypes = {
   onProductSelect: PropTypes.func.isRequired
 };
 
-export default ProductList;
+export default withStyles(styles)(ProductList);
